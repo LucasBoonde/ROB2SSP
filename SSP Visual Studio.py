cca4c01f9db273eee3182bf0b6ae5fa33a9bf5d3
Kaspso <kaspso21@student.aau.dk>
2022-03-17 10:53:22 +0100

    Update SSP Visual Studio.py

--- SSP Visual Studio.py
+++ SSP Visual Studio.py
@@ -1,30 +1,62 @@
 from socket import herror
 
+timestep =?
+
+currentHeight =?
 
 class Motor:
-    def runUntilValueReached(self, runStatus):
-        pass
-    def setMotorSpeed(self, speed):
-        pass
     
+    def runUntilValueReached(self, heightValue):
+        
+        while currentHeight < heightValue:
+            motorRunStatus = True 
+            
+        motorRunStatus = False
+    
+    def setMotorSpeed(self, speed):
+        
+        motorSpeed = speed
+        
+        
 class Encoder:
-    def measureSteps(self, stepsAmount):
-        pass
+    def measureSteps(self):
+        encoder.SensorValue = currentHeight
+        
     def stepToSpeed(self, stepsAmount):
-        stepsAmount omregnes til speed her
-    def stepsToLocation():
-        pass
-    def locationToSteps():
-        pass
+    
+        stepsAmount = #noget matematik så steps omregnes til speed her
+        
+    def stepsToLocation(self, stepsAmount):
+        
+        newHeight = #noget matematik der omregner steps til milimeter hvor stepsAmount indgår
+        
+        currentHeight = newHeight + currentHeight
+    
+    #def locationToSteps():
+        
         
 class Pressure_Sensor:
-    def maxRangeReached():
+    def maxRangeReached(self):
+        if currentHeight = maxHeight:
+            motorRunStatus = False
+    def getPressureValue(self):   
         pass
-    def valueChange():
-        return runStatus
-        pass
+    
+    #def objectLifted(self):
+     #   getPressureValue()
+
+      #  if pressureValue = pressureValue:
+       #     return motorRunStatus = False    
+        
+    
     def detectPressure():
-        pass
+        getPressureValue()
+        
+        if pressureValue != pressureValueOld
+            objectedDetected = True
+
+    
+   
 
     
     
